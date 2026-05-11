297d7eaa408556062f677e76b3b3b5417ed40e36
Akshat Kumar <akshatkumar1010@gmail.com>
2026-05-11 19:38:02 +0530

    Add Comment model with schema definition

---
+++ models/Comment.js
@@ -0,0 +1,33 @@
+// models/comment.js
+
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+  {
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    username: {
+      type: String,
+      required: true,
+    },
+
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Comment", commentSchema);
